7d85684f62399ec9e6b0b32a50618a4cc704a9ac
Joel <joel.torres4@upr.edu>
2017-04-13 12:20:59 -0400

    Some changes...

--- VoyageLex.py
+++ VoyageLex.py
@@ -5,44 +5,53 @@ import ply.lex as lex
 
 # Reserved words
 reserved = {
-   'createpackage': 'CREATEPACKAGE',
-   'destinations': 'DESTINATIONS',
-   'flights': 'FLIGHTS',
-   'fly': 'FLY',
-   'hotels': 'HOTELS',
-   'reserve': 'RESERVE',
-   'stay': 'STAY',
-   'cars': 'CARS',
-   'tours': 'TOURS',
-   'visit': 'VISIT',
-   'book': 'BOOK'
+    'createpackage': 'CREATEPACKAGE',
+    'destinations': 'DESTINATIONS',
+    'flights': 'FLIGHTS',
+    'fly': 'FLY',
+    'hotels': 'HOTELS',
+    'reserve': 'RESERVE',
+    'stay': 'STAY',
+    'cars': 'CARS',
+    'tours': 'TOURS',
+    'visit': 'VISIT',
+    'book': 'BOOK'
 }
 
 # List of token names. This is always required
-tokens = [
-    'NAME',
-    'DESTINATION',
-    'DATE',
-    'AIRLINE',
-    'HOTEL',
-    'RENTAL',
-    'TOUR',
-    'DAYS',
-    'ID'
-] + list(reserved.values())
-
+tokens = ['NAME',
+          'DESTINATION',
+          'DATE',
+          'AIRLINE',
+          'HOTEL',
+          'RENTAL',
+          'TOUR',
+          'DAYS',
+          'ID'
+          ] + list(reserved.values())
 
 # Regular expression rules for simple tokens
 t_NAME = r'[a-zA-Z]+'
+t_DESTINATION = r'[a-zA-Z]+'
 t_DATE = r'/(0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])[- \/.](19|20)\d\d/'
-t_DAYS = r'[0-9]{3}' #DAYS of rental car
+t_AIRLINE = r'[a-zA-Z]+'
+t_HOTEL = r'[a-zA-Z]+'
+t_RENTAL = r'[a-zA-Z]+'
+t_TOUR = r'[a-zA-Z]+'
+
+
+# Define a rule for rental days number
+def t_DAYS(t):
+    r'[0-9]{3}'
+    t.value = int(t.value)
+    return t
 
 
 # Define a rule for reserved words
 def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]+'
-    #r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'ID')    # Check for reserved words
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    print(t.value+" is a keyword!")
+    t.type = reserved.get(t.value, 'ID')  # Check for reserved words
     return t
 
 
@@ -50,6 +59,7 @@ def t_ID(t):
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
+
 
 # A string containing ignored characters (spaces and tabs)
 t_ignore = ' \t'
@@ -59,6 +69,7 @@ t_ignore = ' \t'
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
+
 
 # Build the lexer
 lexer = lex.lex()
